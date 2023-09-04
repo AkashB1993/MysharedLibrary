fd1ca3436217398b607d25d68293e4dfc28835af
AkashB1993 <142805900+AkashB1993@users.noreply.github.com>
2023-09-04 22:35:42 +0530

    Update codes.groovy

--- vars/codes.groovy
+++ vars/codes.groovy
@@ -8,6 +8,5 @@ def newBuild()
 }
 def newDeployment(jobname,ip,appname)
 {
-  deploy adapters: "[tomcat9(credentialsId: '14913ee1-760e-42cd-bd05-0d981085ad17', path: '', url: 'http://${ip})], contextPath: ${appname}, war: '**/*.war"
- // sh "scp /var/lib/jenkinss/workspace/${jobname}/webapp/target/webapp.war ubuntu@ ${ip}:/var/lib/tomcat9/webapp/${appname}.war"
+  sh "scp /var/lib/jenkinss/workspace/${jobname}/webapp/target/webapp.war ubuntu@ ${ip}:/var/lib/tomcat9/webapp/${appname}.war"
 }
