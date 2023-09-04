5f5b224daa275c6078ad550573694805055b5645
AkashB1993 <142805900+AkashB1993@users.noreply.github.com>
2023-09-04 22:05:40 +0530

    Update codes.groovy

--- vars/codes.groovy
+++ vars/codes.groovy
@@ -1,1 +1,12 @@
-def Download
+def gitDownload(repo)
+{
+  git "https://github.com/AkashB1993/${repo}.git"
+}
+def newBuild()
+{
+  sh 'mvn package'
+}
+def newDeployment(jobname,ip,appname)
+{
+  
+}
