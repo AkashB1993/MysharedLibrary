e1fcaeabf0048b9986f395f08f5b27ef075a54af
AkashB1993 <142805900+AkashB1993@users.noreply.github.com>
2023-09-05 21:33:28 +0530

    Update codes.groovy

--- vars/codes.groovy
+++ vars/codes.groovy
@@ -10,4 +10,9 @@ def newDeployment(jobname,ip,appname)
 {
 sh "scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
 }
+def runSelenium()
+{
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
+
  
