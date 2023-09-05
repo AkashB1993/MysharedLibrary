0c531b18c8c4674c1faf00623f38929b365c6701
AkashB1993 <142805900+AkashB1993@users.noreply.github.com>
2023-09-05 18:18:27 +0530

    Update codes.groovy

--- vars/codes.groovy
+++ vars/codes.groovy
@@ -9,4 +9,5 @@ def newBuild()
 def newDeployment(jobname,ip,appname)
 {
 sh "scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
 
