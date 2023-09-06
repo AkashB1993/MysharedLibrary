14a136f7bc6e15ccf64f2724eab86926f3cf1ed7
AkashB1993 <142805900+AkashB1993@users.noreply.github.com>
2023-09-06 12:45:29 +0530

    Update codes.groovy

--- vars/codes.groovy
+++ vars/codes.groovy
@@ -13,7 +13,7 @@ sh "scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubun
 def runSelenium(jobname)
 {
   sh "java -jar /home/ubuntu/.jenkins/workspace/${jobname}/testing.jar"
-  ///home/ubuntu/.jenkins/workspace/DecltiveSharedLibrary/.git
+  
 }
 
  
